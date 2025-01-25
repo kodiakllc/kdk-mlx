09f5e47bee497179795a0c020e886137ab04dc39
Nick Jaunich <panayao@gmail.com>
2025-01-25 16:32:54 -0400

    chore(KDK-999): Use correct model string.

--- py/DeepSeekR1.Qwen.32b.py
+++ py/DeepSeekR1.Qwen.32b.py
@@ -9,7 +9,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 MODELS_PATH = os.path.join(SCRIPT_DIR, "../hf_models/")
 
 # Set the current model
-CURRENT_T_MODEL = "QwQ-32B-Preview-4bit"
+CURRENT_T_MODEL = "DeepSeek-R1-Distill-Qwen-32B-4bit"
 
 # Init the model and tokenizer
 model = None
